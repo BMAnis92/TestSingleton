79d0f0232af01e5150d6c98f4a642b2627b648b5
BMAnis92 <anis.ben.meddeb@gmail.com>
2024-06-20 17:38:16 -0400

    v2

--- src/Singleton.java
+++ src/Singleton.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Singleton {
+public class Singleton {
+
+    private int nbrExection =0;
+    //instance
+    private static Singleton instance = null;
+
+    //Singlton
+
+    public Singleton() {
+        System.out.println("Creating Singletonn");
+        nbrExection++;
+    }
+
+    //getInstance
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
 }
